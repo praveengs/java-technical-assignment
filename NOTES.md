fbf926117b2e776768711d08cbde723b7ddbc347
praveengs <praveengs@gmail.com>
2020-03-02 00:55:52 +0000

    Basket integrated with discount rule calculator to subtract discounts for buy one get one.

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,7 @@
 # Notes
 
 Please add here any notes, assumptions and design decisions that might help up understand your though process.
+
+# TODO
+There should be a better way to initialise the rules, rather than passing a list of discount rules into the 
+basket constructor. But for now, this is convenient.
--- src/test/java/kata/supermarket/BasketTest.java
+++ src/test/java/kata/supermarket/BasketTest.java
@@ -1,13 +1,16 @@
 package kata.supermarket;
 
+import kata.supermarket.rules.BuyOneGetOneDiscountRule;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,8 +20,8 @@ class BasketTest {
     @DisplayName("basket provides its total value when containing...")
     @MethodSource
     @ParameterizedTest(name = "{0}")
-    void basketProvidesTotalValue(String description, String expectedTotal, Iterable<Item> items) {
-        final Basket basket = new Basket();
+    void basketProvidesTotalValue(String description, List<DiscountRule> discountRules, String expectedTotal, Iterable<Item> items) {
+        final Basket basket = new Basket(discountRules);
         items.forEach(basket::add);
         assertEquals(new BigDecimal(expectedTotal), basket.total());
     }
@@ -29,31 +32,41 @@ class BasketTest {
                 aSingleItemPricedPerUnit(),
                 multipleItemsPricedPerUnit(),
                 aSingleItemPricedByWeight(),
-                multipleItemsPricedByWeight()
+                multipleItemsPricedByWeight(),
+                multipleItemsWithDiscount()
         );
     }
 
     private static Arguments aSingleItemPricedByWeight() {
-        return Arguments.of("a single weighed item", "1.25", Collections.singleton(twoFiftyGramsOfAmericanSweets()));
+        return Arguments.of("a single weighed item", new ArrayList<>(), "1.25", Collections.singleton(twoFiftyGramsOfAmericanSweets()));
     }
 
     private static Arguments multipleItemsPricedByWeight() {
-        return Arguments.of("multiple weighed items", "1.85",
+        return Arguments.of("multiple weighed items", new ArrayList<>(), "1.85",
                 Arrays.asList(twoFiftyGramsOfAmericanSweets(), twoHundredGramsOfPickAndMix())
         );
     }
 
+    private static Arguments multipleItemsWithDiscount() {
+        return Arguments.of("items with discounts", createAllDiscountRules(), "0.49",
+                Arrays.asList(aPintOfMilk(), aPintOfMilk()));
+    }
+
+    private static List<DiscountRule> createAllDiscountRules() {
+        return Collections.singletonList(new BuyOneGetOneDiscountRule("Milk"));
+    }
+
     private static Arguments multipleItemsPricedPerUnit() {
-        return Arguments.of("multiple items priced per unit", "2.04",
+        return Arguments.of("multiple items priced per unit", new ArrayList<>(), "2.04",
                 Arrays.asList(aPackOfDigestives(), aPintOfMilk()));
     }
 
     private static Arguments aSingleItemPricedPerUnit() {
-        return Arguments.of("a single item priced per unit", "0.49", Collections.singleton(aPintOfMilk()));
+        return Arguments.of("a single item priced per unit", new ArrayList<>(), "0.49", Collections.singleton(aPintOfMilk()));
     }
 
     private static Arguments noItems() {
-        return Arguments.of("no items", "0.00", Collections.emptyList());
+        return Arguments.of("no items", new ArrayList<>(), "0.00", Collections.emptyList());
     }
 
     private static Item aPintOfMilk() {
